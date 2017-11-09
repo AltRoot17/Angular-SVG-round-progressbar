63b56b2ccc0e9624cb3746e7fa3076809cd91d64
Madhawa Bandara <madhawa_b@hotmail.com>
2017-11-09 15:50:31 +0530

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+})
+export class HomePage {
+  current: number = 27;
+  max: number = 50;
+  stroke: number = 15;
+  radius: number = 125;
+  semicircle: boolean = false;
+  rounded: boolean = false;
+  responsive: boolean = false;
+  clockwise: boolean = true;
+  color: string = '#45ccce';
+  background: string = '#eaeaea';
+  duration: number = 800;
+  animation: string = 'easeOutCubic';
+  animationDelay: number = 0;
+  animations: string[] = [];
+  gradient: boolean = false;
+  realCurrent: number = 0;
+  rate: number;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+}
